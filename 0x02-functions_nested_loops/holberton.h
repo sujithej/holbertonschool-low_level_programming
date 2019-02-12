969d9d341332ab09993454e37f629cbe3504e513
snehadasa <sneha.dasasneha@gmail.com>
2019-02-12 07:44:36 -0800

    header files

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -5,4 +5,13 @@
 
 int _putchar(char c);
 
+// Prints charecter a-z in one line
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
 #endif
