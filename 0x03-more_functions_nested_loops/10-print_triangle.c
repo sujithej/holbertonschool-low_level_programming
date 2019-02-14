2e0e46b369c09ce896bb69811b3dd01366309d66
snehadasa <sneha.dasasneha@gmail.com>
2019-02-14 14:48:31 -0800

    printing triangles

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - print triangle of characters.
+ * @size: number of times the character to print.
+ * return
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 1; i <= size; i++)
+	{
+		for (j = i; j < size; j++)
+		{
+			_putchar(' ');
+		}
+		for (j = 1; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
