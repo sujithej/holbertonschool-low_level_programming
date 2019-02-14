234684ffd90e2066c0758168f4cca7d15cd6f26a
snehadasa <sneha.dasasneha@gmail.com>
2019-02-14 15:36:39 -0800

    printing diagonal

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - printing diagonal of spaces and \.
+ * @n: n is the number of times # has to print.
+ * Return: 0 if success.
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+	_putchar('\n');
+	}
+}
