ac89f2060dd2c115da3b99688a47ed0bc279f65e
snehadasa <sneha.dasasneha@gmail.com>
2019-02-19 15:55:48 -0800

    prints string

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _puts - prints a string.
+ * @str: string.
+ * Return: 0 if success.
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
+}
