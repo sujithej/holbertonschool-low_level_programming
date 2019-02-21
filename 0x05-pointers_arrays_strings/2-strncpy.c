aafe7492e879f0f317a0903593f0134cf061e1b0
snehadasa <sneha.dasasneha@gmail.com>
2019-02-21 08:53:15 -0800

    copy 2 strings

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * strlen_1 - gives the length of a string.
+ * @s: string.
+ * Return: 0 if success.
+ */
+int strlen_1(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		s++;
+	}
+	return (i);
+
+}
+
+/**
+ * *_strncpy - copy strings.
+ * @dest: destination where src is copied into.
+ * @src: source file.
+ * @n: till n variables.
+ * Return: 0 if success.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
