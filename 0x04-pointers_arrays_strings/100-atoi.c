603302db478392527041c99ca3edcc9ffb44575c
snehadasa <sneha.dasasneha@gmail.com>
2019-02-20 20:33:56 -0800

    atoi

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * _atoi - converts string to an integer.
+ * @s: string.
+ * Return: 0 if success.
+ */
+int _atoi(char *s)
+{
+	int i;
+	int sign = 1;
+	int flag = 0;
+	int result = 0;
+
+	for (i = 0; *s != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (flag == 1)
+		{
+			flag = 2;
+		}
+	}
+	return (sign * result);
+}
+
