cce450b61d0f4d64a8b0fff2d9bb1a640fc8caff
snehadasa <sneha.dasasneha@gmail.com>
2019-02-21 17:17:19 -0800

    copy string

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -26,11 +26,19 @@ int strlen_1(char *s)
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
+	}
+
+	if (i < n)
+	{
+		for (j = i; j < n; j++)
+		{
+			dest[j] = '\0';
+		}
 	}
 
 	return (dest);
