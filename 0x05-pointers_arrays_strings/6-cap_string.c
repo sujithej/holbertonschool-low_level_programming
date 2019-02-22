bf646a1ea05f88b4622928f18497e3896e6ffbec
snehadasa <sneha.dasasneha@gmail.com>
2019-02-21 19:21:23 -0800

    betty errors

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -1,27 +1,26 @@
 #include "holberton.h"
 
 /**
-* isSeperator - checks if it is a seperator.
-* @c: character to check. 
+* isSeparator - checks if it is a seperator.
+* @c: character to check.
 * Return: capitalized string.
 */
-
 int isSeparator(char *c)
 {
 	int i;
 	char sep[] = {' ', '\t', '\n', ',', ';', '.',
-	       	'!', '?', '"', '(', ')', '{', '}'};
-	
+			'!', '?', '"', '(', ')', '{', '}'};
+
 	for (i = 0; i < (int) sizeof(sep); i++)
 	{
 		if (*c == sep[i])
 		{
-			return 1;
+			return (1);
 		}
 	}
-	return 0;
+	return (0);
 }
-	
+
 
 /**
 * cap_string - capitalize all words in a string.
@@ -33,16 +32,16 @@ char *cap_string(char *str)
 {
 	int i;
 
-	
-	if (*str >= 'a' && *str <= 'z')
-        {
-                *str = *str - 32;
-        }
 
-	
+	if (*str >= 'a' && *str <= 'z')
+	{
+		*str = *str - 32;
+	}
+
+
 	for (i = 1; str[i]; i++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z' && isSeparator(&str[i-1]))
+		if (str[i] >= 'a' && str[i] <= 'z' && isSeparator(&str[i - 1]))
 		{
 			str[i] = str[i] - 32;
 		}
