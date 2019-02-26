64e8e0fa0fc5e4c878662e6f8fc9ddb85598d6de
snehadasa <sneha.dasasneha@gmail.com>
2019-02-25 17:51:10 -0800

    haystack and needle

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * strchr_1 - locates character in string
+ * @c: character c to be located for.
+ * @s: pointer to a string.
+ * Return: returns true if it matches.
+ */
+char strchr_1(char *s, char *c)
+{
+	while (*s != '\0')
+	{
+		if (*s != *c)
+			return (0);
+		s++;
+		c++;
+	}
+
+	return (1);
+}
+
+/**
+ * _strstr - searches string for a substring
+ * @haystack: string to search for needle
+ * @needle: the substring to find
+ * Return: pointer to first matching substring or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	if (*needle == '\0')
+		return (haystack);
+
+	while (*haystack != '\0')
+	{
+		if (strchr_1(haystack, needle))
+		return (haystack);
+		haystack++;
+	}
+	return (NULL);
+}
