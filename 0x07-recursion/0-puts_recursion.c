a636cff294d8c65992701859a182e51dd2f2ad88
snehadasa <sneha.dasasneha@gmail.com>
2019-02-27 16:42:04 -0800

    new

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -7,12 +7,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
+	if (*s)
+	{ 
 		_putchar(*s);
 
                 _puts_recursion(++s);
+
+	}
+	else
+		_putchar('\n');
 }
