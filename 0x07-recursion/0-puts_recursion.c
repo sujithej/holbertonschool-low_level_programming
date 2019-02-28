51956bc77d7d9af68f795835eaee6f04cd9186f2
snehadasa <sneha.dasasneha@gmail.com>
2019-02-27 16:29:21 -0800

    updated

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -1,18 +1,17 @@
 #include "holberton.h"
 
 /**
- * _puts_recursion - prinys a string.
+ * _puts_recursion - printis a string.
  * @s: sting.
  * Return: void.
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(++s);
 	}
-	_putchar(*s);
-
-	_puts_recursion(++s);
+	else
+		_putchar('\n');
 }
