1b0d90dba53fe0c4fc2d00fcd5223e86d59eca89
snehadasa <45522052+snehadasa@users.noreply.github.com>
2019-03-01 16:20:26 -0800

    Create 100-wildcmp.c

---
+++ 0x07-recursion/100-wildcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compares two strings lexicographically
+ * @s1: the first string
+ * @s2: the second string, can contain wildcard
+ *
+ * Return: 1 if identical, 0 if false
+ */
+int wildcmp(char *s1, char *s2)
+{
+if (*s1 == '\0')
+{
+  if (*s2 != '\0' && *s2 == '*')
+  return (wildcmp(s1, s2 + 1));
+  return (*s2 == '\0');
+}
+if (*s2 == '*')
+return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+else if (*s1 == *s2)
+return wildcmp(s1 + 1, s2 + 1));
+return (0);
+}
