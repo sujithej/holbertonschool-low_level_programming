04a2d1ac77e29a69c58049eb37c9ded161c7a191
snehadasa <sneha.dasasneha@gmail.com>
2019-03-02 20:11:57 -0800

    add

---
+++ 0x09-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+int is_digit(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if ((s[i] <= '0' || s[i] >= '9') && !(i == 0 && s[i] == '-'))
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+/**
+ * main - starting point.
+ * @argc: the number of arguments
+ * @argv: array of pointers to arguments
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc <= 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!is_digit(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
