7df8179a8a52e183109c08712006173313266641
snehadasa <sneha.dasasneha@gmail.com>
2019-03-02 17:40:39 -0800

    argc num of arguments passed

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * main - starting point.
+ * @argc: the number of arguments
+ * @argv: array of pointers to arguments
+ * Return: Always 0(success).
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
