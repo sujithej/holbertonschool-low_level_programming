8681bcfdb13beb84797aef326755c1b791bf29c2
snehadasa <sneha.dasasneha@gmail.com>
2019-03-02 17:33:29 -0800

    printing its own name

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * main - starting point.
+ * @argc: the number of arguments
+ * @argv: array of pointers to arguments
+ * Return: Always 0(success).
+ */
+int main(int argc, char *argv[])
+{
+	if (argc)
+		printf("%s\n", argv[0]);
+	return (0);
+}
