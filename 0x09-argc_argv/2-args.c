a63f958c13d1a6ec71c208fc305b68a07d4084c4
snehadasa <sneha.dasasneha@gmail.com>
2019-03-02 17:45:36 -0800

    printing all the arguments that are entered

---
+++ 0x09-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - starting point.
+ * @argc: the number of arguments
+ * @argv: array of pointers to arguments
+ * Return: Always 0(success).
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
